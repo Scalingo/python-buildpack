967edf9c22b3feeb05691a05db6a55d42948bd4e
François KUBLER <francois+github@kubler.org>
2026-02-25 16:46:51 +0100

    fix(sqlalchemy): add compat with pyproject.toml

--- vendor/python.sqlalchemy.sh
+++ vendor/python.sqlalchemy.sh
@@ -1,25 +1,20 @@
 #!/usr/bin/env bash
 
 set_sql_alchemy_url() {
-  local database_url=$1
-  local environment_variables_prefix=$2
+	local database_url="${1}"
+	local environment_variables_prefix="${2}"
 
-  local pattern="sqlalchemy"
-  concerned1=$(cat "requirements.txt" | grep -ic $pattern)
-  concerned2=$(cat "Pipfile" | grep -ic $pattern)
-  if [ $concerned1 -eq 0 ] && [ $concerned2 -eq 0 ]; then
-    return 0
-  fi
+	if grep --ignore-case --silent "sqlachemy" \
+		"requirements.txt" "Pipfile" "pyproject.toml" \
+		&& test "${database_url}" =~ "postgres://"
+	then
+		# Replace 'postgres://' with 'postgresql://':
+		local full_database_url="${database_url/postgres:\/\//postgresql://}"
 
-  if ! [[ $database_url =~ postgres:// ]];then
-    return 0
-  fi
-
-  local full_database_url
-  full_database_url="${database_url/postgres:\/\//postgresql://}"
-  eval "export ${environment_variables_prefix}_URL=$full_database_url"
+		eval "export ${environment_variables_prefix}_URL=${full_database_url}"
+	fi
 }
 
-for database_url_variable in $(env | awk -F "=" '{print $1}' | grep "SCALINGO_POSTGRESQL_URL"); do
-  set_sql_alchemy_url "$(eval echo "\$${database_url_variable}")" "${database_url_variable//_URL/}_ALCHEMY"
+for database_url_variable in $( env | awk -F "=" '{print $1}' | grep "SCALINGO_POSTGRESQL_URL" ); do
+	set_sql_alchemy_url "$( eval echo "\$${database_url_variable}" )" "${database_url_variable//_URL/}_ALCHEMY"
 done
