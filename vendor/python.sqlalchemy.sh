fae260750d1ff1f6a0a6b9b1598032f4fd358da5
François KUBLER <francois+github@kubler.org>
2026-02-26 10:15:28 +0100

    fix(sqlalchemy): improve profile.d script
    - Fix the conditional
    - Remove a dangerous `eval` call
    - Rewrote some variables logic

--- vendor/python.sqlalchemy.sh
+++ vendor/python.sqlalchemy.sh
@@ -2,19 +2,19 @@
 
 set_sql_alchemy_url() {
 	local database_url="${1}"
-	local environment_variables_prefix="${2}"
+	local env_var="${2}"
 
-	if grep --ignore-case --silent "sqlachemy" \
+	if grep --ignore-case --silent --no-messages "sqlalchemy" \
 		"requirements.txt" "Pipfile" "pyproject.toml" \
-		&& test "${database_url}" =~ "postgres://"
+		&& [[ "${database_url}" =~ "postgres://" ]]
 	then
 		# Replace 'postgres://' with 'postgresql://':
 		local full_database_url="${database_url/postgres:\/\//postgresql://}"
 
-		eval "export ${environment_variables_prefix}_URL=${full_database_url}"
+		export "${env_var}=${full_database_url}"
 	fi
 }
 
 for database_url_variable in $( env | awk -F "=" '{print $1}' | grep "SCALINGO_POSTGRESQL_URL" ); do
-	set_sql_alchemy_url "$( eval echo "\$${database_url_variable}" )" "${database_url_variable//_URL/}_ALCHEMY"
+	set_sql_alchemy_url "$( eval echo "\$${database_url_variable}" )" "${database_url_variable//_URL/}_ALCHEMY_URL"
 done
